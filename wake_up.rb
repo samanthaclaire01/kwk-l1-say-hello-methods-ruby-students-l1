73fece16e11105a92679a2d00481163ba79d3046
Samantha Cassity <cassityclaire@gmail.com>
2018-06-05 16:53:55 +0000

    Done.

---
+++ wake_up.rb
@@ -0,0 +1,11 @@
+def wake_up
+  puts "Brushes my teeth"
+  puts "Washes my face"
+  puts "Makes a cup of tea"
+  2.times do
+  puts "Looks for an outfit"
+end
+  puts "Gets dressed"
+  puts "Brushes my hair"
+end 
+wake_up

a63e1607fcc2f9248891fbec4f6fbb7cb3fb84fe
Samantha Cassity <cassityclaire@gmail.com>
2018-06-05 16:52:48 +0000

    automatically backed up by learn

--- wake_up.rb
+++ wake_up.rb
@@ -2,9 +2,9 @@ def wake_up
   puts "Brushes my teeth"
   puts "Washes my face"
   puts "Makes a cup of tea"
+  2.times do
+  puts "Looks for an outfit"
   puts "Gets dressed"
-  2.times do 
   puts "Brushes my hair"
-end 
 end 
 wake_up

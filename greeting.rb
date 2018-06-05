c5a312eb0253ff37e96653687fb5ffdacca9a988
Samantha Cassity <cassityclaire@gmail.com>
2018-06-05 17:08:20 +0000

    Done.

---
+++ greeting.rb
@@ -0,0 +1,4 @@
+def greeting(local_greeting, your_name, my_name, time_of_day)
+  puts "#{local_greeting} #{your_name}, I am #{my_name}, how's your #{time_of_day}?"
+end
+greeting("Hey", "Esme", "Sam", "Morning")
